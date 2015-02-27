c886b3283d53fec68dd4380f19d0be4bdccdfbf8
Brad Bell <brad@pixelandtonic.com>
2015-02-26 17:52:30 -0800

    Show all errors.

--- server/checkit.php
+++ server/checkit.php
@@ -1,5 +1,9 @@
 <?php
 
+// Turn it all on.
+error_reporting(E_ALL|E_STRICT);
+ini_set('display_errors','1');
+
 require_once(__DIR__.'/requirements/RequirementsChecker.php');
 
 $checker = new RequirementsChecker();
