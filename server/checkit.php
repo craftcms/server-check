02cb76e9e39927f37a581905cb226d682910d08a
Tim Kelty <tim@pixelandtonic.com>
2021-08-16 15:04:10 -0400

    Return non-zero code on error

--- server/checkit.php
+++ server/checkit.php
@@ -8,3 +8,13 @@ require_once(__DIR__.'/requirements/RequirementsChecker.php');
 
 $checker = new RequirementsChecker();
 $checker->checkCraft()->render();
+$args = isset($_SERVER['argv']) ? $_SERVER['argv'] : [];
+$strict = in_array('--strict', $args);
+
+if ($checker->result['summary']['errors'] ||
+    ($strict && $checker->result['summary']['warnings'])
+) {
+    exit(1);
+}
+
+exit(0);
