1251fb86efa3d15a940675b655fe65da098be29f
Tim Kelty <tim@pixelandtonic.com>
2021-08-16 15:14:02 -0400

    Rename to make sense in context of composer/bin

--- server/checkit.php
+++ server/checkit.php
@@ -5,17 +5,4 @@
 error_reporting(E_ALL | E_STRICT);
 @ini_set('display_errors', '1');
 
-require_once(__DIR__.'/requirements/RequirementsChecker.php');
-
-$checker = new RequirementsChecker();
-$checker->checkCraft()->render();
-$args = isset($_SERVER['argv']) ? $_SERVER['argv'] : [];
-$strict = in_array('--strict', $args);
-
-if ($checker->result['summary']['errors'] ||
-    ($strict && $checker->result['summary']['warnings'])
-) {
-    exit(1);
-}
-
-exit(0);
+require_once(__DIR__.'/craftcms-server-check');
