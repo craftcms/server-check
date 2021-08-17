35cac2573430b7ea24e463f060286e7976619333
Tim Kelty <tim@pixelandtonic.com>
2021-08-17 10:22:02 -0400

    Return error codes

--- server/checkit.php
+++ server/checkit.php
@@ -5,4 +5,17 @@
 error_reporting(E_ALL | E_STRICT);
 @ini_set('display_errors', '1');
 
-require_once(__DIR__.'/craftcms-server-check');
+require_once(__DIR__.'/requirements/RequirementsChecker.php');
+
+$checker = new RequirementsChecker();
+$checker->checkCraft()->render();
+
+$args = isset($_SERVER['argv']) ? $_SERVER['argv'] : [];
+$strict = in_array('--strict', $args);
+
+if ($checker->result['summary']['errors'] || ($strict && $checker->result['summary']['warnings'])
+) {
+    exit(1);
+}
+
+exit(0);
